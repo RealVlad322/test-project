351144a7a7a248263de13eb20c927f4377c3655f
Vladislav Kanishchev <ridgi2003@gmail.com>
2024-08-30 22:45:38 +0300

    fix bugs

--- src/shared/contract/mapper/mstuca-shedule-mapper.service.ts
+++ src/shared/contract/mapper/mstuca-shedule-mapper.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import groupBy from 'lodash/groupBy';
 
 import { CreateSheduleDto } from '../../dtos';
 import { MstucaResponse } from '../mstuca-api/types';
@@ -8,33 +7,28 @@ import { MstucaResponse } from '../mstuca-api/types';
 export class MstucaSheduleMapperService {
   getAllMappedShedule(fullStat: MstucaResponse[]): CreateSheduleDto[] {
     const result: CreateSheduleDto[] = [];
+    fullStat.forEach((sub) => {
+      const shedule: CreateSheduleDto = {
+        date: sub.date,
+        grade: 4,
+        group: 1,
+        discipline: '',
+        faculty: '',
+        groupName: '',
+        index: 0,
+        type: '',
+        place: '',
+        teacher: '',
+      };
+      shedule.discipline = sub.discipline;
+      shedule.type = sub.kindOfWork;
+      shedule.place = sub.auditorium;
+      shedule.index = sub.lessonNumberStart;
+      shedule.teacher = sub.lecturer;
+      shedule.address = sub.building;
+      shedule.groupName = sub.stream;
 
-    const groupedStat = Object.entries(groupBy(fullStat, 'date'));
-
-    groupedStat.forEach(([date, item]) => {
-      item.forEach((sub) => {
-        const shedule: CreateSheduleDto = {
-          date,
-          grade: 4,
-          group: 1,
-          discipline: '',
-          faculty: '',
-          groupName: '',
-          index: 0,
-          type: '',
-          place: '',
-          teacher: '',
-        };
-        shedule.discipline = sub.discipline;
-        shedule.type = sub.kindOfWork;
-        shedule.place = sub.auditorium;
-        shedule.index = sub.lessonNumberStart;
-        shedule.teacher = sub.lecturer;
-        shedule.address = sub.building;
-        shedule.groupName = sub.stream;
-
-        result.push(shedule);
-      });
+      result.push({ ...sub, ...shedule });
     });
 
     return result;
